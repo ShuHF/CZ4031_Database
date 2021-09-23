45a6466f73edc8539c4794a938424e84eaa6b77a
ShuHf <fang_shu_hui@outlook.com>
2021-09-23 19:21:59 +0800

    Gui

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  cout << "Hello World!";
-  return 0;
+int main()
+{
+    cout << "*********************************************************|" << endl;
+    cout << "| 1) Check Database size                                 |" << endl;
+    cout << "| 2) Number of block used                                |" << endl;
+    cout << "| 3) Set block size                                      |" << endl;
+    cout << "| 4) Build B+ tree on 'numVote'                          |" << endl;
+    cout << "| 5) Retrieve movies 'numVotes' = 500                    |" << endl;
+    cout << "| 6) Retrieve movies 'numVotes' between to 30000 to 40000|" << endl; 
+    cout << "| 7) Delete movies 'numVotes' = 1000                     |" << endl;
+    cout << "*********************************************************|" << endl;
 }
