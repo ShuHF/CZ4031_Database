f9fd859b3446c9e21d09f4993c1f8ef39c6e3290
musifahamran <49061096+musifahamran@users.noreply.github.com>
2021-09-23 19:10:27 +0800

    Update Storage.cpp

--- Storage.cpp
+++ Storage.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
+
 class Storage {     // The class
   public:           // Access specifier
-    int diskCapacity;
-    int blockSize;
-    Storage(int diskCapacity, int blockSize);
+    int blockSize=100;
+    string disk [100000000];
+    Storage(int blockSize) {     // Constructor
+      blockSize = blockSize;
+    }
+    public: 
+
+      int getDiskCapacitySize()
+      {
+        return sizeof(disk);
+      }
+
+      int getDatabaseSize()
+      {
+        //Calcaluation added later
+        return getDiskCapacitySize()-blockSize;
+      }
+
+      void setBlockSize(int block)
+      {
+          blockSize = block;
+      }
+
+      int getBlockSize()
+      {
+          return blockSize;
+      }
+      void readData()
+      {
+          
+      }
+
+
 };   
 
-    Storage::Storage(int diskCapacity, int blockSize) {     // Constructor
-      cout << "Hello World!";
-    }
+    
     
