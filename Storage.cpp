bf70d5e89ff5db4a9945234f3d100e1ca66ee546
musifahamran <49061096+musifahamran@users.noreply.github.com>
2021-09-23 17:59:54 +0800

    Update Storage.cpp

--- Storage.cpp
+++ Storage.cpp
@@ -3,9 +3,10 @@ class Storage {     // The class
   public:           // Access specifier
     int diskCapacity;
     int blockSize;
+    Storage(int diskCapacity, int blockSize);
+};   
 
-    Storage() {     // Constructor
+    Storage::Storage(int diskCapacity, int blockSize) {     // Constructor
       cout << "Hello World!";
     }
     
-};
