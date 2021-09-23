35cdf6898cdc138a2b484fd684354c25b74dedce
musifahamran <49061096+musifahamran@users.noreply.github.com>
2021-09-23 17:54:28 +0800

    preview

---
+++ Storage.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+class Storage {     // The class
+  public:           // Access specifier
+    int diskCapacity;
+    int blockSize;
+
+    Storage() {     // Constructor
+      cout << "Hello World!";
+    }
+    
+};
