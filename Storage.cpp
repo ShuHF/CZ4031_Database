f1118b46023fd8d2e0671e19737a510f68dababa
ShuHf <fang_shu_hui@outlook.com>
2021-09-26 02:11:47 +0800

    Edited the codes  for linking  the data into the storage disk

--- Storage.cpp
+++ Storage.cpp
@@ -2,111 +2,104 @@
 #include <fstream>
 #include <string>
 #include <vector>
-// #include <boost/algorithm/string.hpp>
 #include "Storage.h"
 using namespace std;
 
+/*block size to either 100Mb/ 500Mb
+ *disk size = disk storage or disk capacity
+ * blockid = which record is assign to which block
+ * currentblockid = for checking size of block
+ * numberofavaliableblock = find out how many block can be created based on the disksize
+ * currentblockSize = get the current block size that is using
+ * totalSpaceUsed = total space used up by the record*/
 int blockSize=100;
-unsigned char * diskspace;
+  int disksize = 100000000;
+  int blockid = 0;
+  int currentblockid = 0;
+  int numberofavaliableblock = disksize / blockSize;
+  int currentblockSize = 0;
+  int totalSpaceUsed = 0;
 
-Storage::Storage(int blockSize) {     // Constructor
-  this->blockSize = blockSize;
-  diskspace = ( unsigned char * )malloc( 100000000 * sizeof( unsigned char ) );
-}
-
-int Storage::getDiskCapacitySize()
+//Constructor to initialize the class
+Storage::Storage(int disksize, int blocksize )
 {
-  return sizeof(diskspace);
+  this->blockSize=100;
+  this->disksize = 100000000;
+  this->blockid = 0;
+  this->currentblockid = 0;
+  this->numberofavaliableblock = disksize / blockSize;
+  this->currentblockSize = 0;
+  this->totalSpaceUsed = 0;
 }
 
-int Storage::getDatabaseSize()
+/*
+ *This function is used to figure out how to store the record in the diskplace based on the 4 scenario
+ *1. Reject if the record is bigger then the blocksize specified
+ *2. Reject if there is no more avaliable block size
+ *3. Add the record into a new block if the record cant fit in the current block
+ *4. Add the record into current block 
+ *
+ * According to the lecture to refer to a record required address, block id
+ **/
+void storeRecordintoBlock(int recordSize)
 {
-  //Calcaluation added later
-  return Storage::getDiskCapacitySize()-blockSize;
-}
-
-void Storage::setBlockSize(int block)
-{
-  this->blockSize = block;
-}
-
-int Storage::getBlockSize()
-{
-  return blockSize;
-}
-int Storage::getNumBlock()
-{
-  return 10000000/this->blockSize;
-}
-// void Storage::readData() //for by line -> issue string spilt
-// {
-//   string line;
-//   int count =0;
-//   char character = 0;
-//   char temp;
-//   vector<char> v;
-//   vector<string> parts;
-//   ifstream filename("C:\\data.tsv");
-//   filename.ignore(10000,'\n');
-  
-//   while (getline (filename, line)) {
-//   // Output the text from the file
-    
-//     split(parts, line, boost::is_any_of("\t"));
-
-//     for(int i=0; i<parts[i].size(); i++){
-//       // char char_array[parts[i].length()];
-//       // strcpy(char_array, parts[i].c_str());
-//       copy(parts[i].begin(), parts[i].end(), back_inserter(v));
-//        for (int k = 0; k < v.size(); k++){
-//          diskspace[count]=v[k];
-//          count++;
-//        }
-//        v.clear();
-        
-//     }
-//     parts.clear();
-    
-    
-//   }
-//   filename.close();
-// }
-// void Storage::readData(){ //read by char -> issue havent ignored the headers
-//   string myText;
-//   int count =0;
-//   unsigned char character=0;
-//   string filename("C:\\data.tsv");
-
-// FILE* input_file = fopen(filename.c_str(), "r");
-//     if (input_file == nullptr) {
-//        cout <<"Failure to read" <<endl;
-//     }
-// while (!feof(input_file)) {
-//        character = getc(input_file);
-//       if(count>29) //temp solution to ignore headers
-//        diskspace[count-30]=character;
-//       count++;
-//     }
-
-// }
-
-void Storage::freeDisk(){
-  free(diskspace);
-}
-
-//print all the records in database, Change condition from NULL to any number for content checking
-void Storage::printContents(){ 
-  int diskSize = getDiskCapacitySize();
-  int i=0;
-  do{
-      cout << diskspace[i];
-      i++;
-  }while(i<60);  //change to i < 60 for 3 record showing, last tconstant is tt9916778 -> (diskspace[i] != NULL)
+  if(recordSize == 0)
+  {
+    cout<< "No record to be added!"<< endl;
   }
+  //Scenario 1 where the record size is greater then the blocksize
+  if(recordSize > blockSize)
+  {
+    cout<< "The record size excceed the block size specified!" <<endl;
+  }
+  //Scenario 2 if record size cant be fit in the current blk and there are no more avaliable block 
+  else if(((currentblockSize + recordSize) > blockSize) && (currentblockid == numberofavaliableblock))
+  {
+    cout<< "Unable to store record as there are no avaliable space in the disk!" <<endl;
+  }
+  //scenario 3 if the record size + current block id size is more than the block size and there are avaliable block
+  else if((currentblockSize + recordSize) > blockSize)
+  {
+    //Change to new block 
+    currentblockid ++;
+    /*
+    * CODES HERE TO STORE IT
+    */
+    numberofavaliableblock --;
+    currentblockSize = 0;
+    currentblockSize += recordSize;
+  }
+ //Scenario 4 ample space to fit the record in the current block without exceeding the block size specified 
+ else
+ {
+   /**
+    * CODES HERE TO STORE IT INTO THE BLOCK
+    * */
+   currentblockSize += recordSize;
+ }
+ totalSpaceUsed += recordSize;
+}
 
-  
+/* 
+*This function is returning number of avaliable blocks left (excluding the current one)
+*/
+int avaliableBlockleft()
+{
+  return numberofavaliableblock;
+}
 
+/*
+*The total space used up by storing the record
+*/
+int databasesize()
+{
+  return totalSpaceUsed;
+}
 
-   
-
-    
+/**
+ * This function is return the number of blocks used up (including current block)
+ **/
+int numBlockUsed()
+{
+   return currentblockid + 1;
+}
