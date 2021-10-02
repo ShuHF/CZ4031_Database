eaf759247ade010e7fdb0d0ea5d73fea78695860
ShuHf <fang_shu_hui@outlook.com>
2021-10-03 05:44:56 +0800

    Edited to allow printing of index node + printing the data record from the index node

--- bplus-tree-search.cpp
+++ bplus-tree-search.cpp
@@ -5,8 +5,11 @@
 using namespace std;
 
 Node* BPTree::search(int key, bool flag, bool rangeflag, int key2){
+    int countingnode = 0;
+    int printonce = 0;
+    int preholder =0;
+    int loopcounter =0;
     // if tree is empty
-    cout << "Finding what key: " << to_string(key) <<endl;
     if (root == NULL)
         cout << "B+ Tree is empty!\n";
     else // else, traverse through the nodes
@@ -19,11 +22,70 @@ Node* BPTree::search(int key, bool flag, bool rangeflag, int key2){
             for(int i=0; i < cursor->size; i++){
                 if(key < cursor->keys[i].value)
                 {
+                    if(flag == true)
+                    {       //this is to get the internal node 
+                            //need to reject taking in root node (first checking) into considertaion
+                            if(preholder == 0)
+                            {
+                                preholder++;
+                            }
+                            else
+                            {   
+                                for (int j = 0; j < cursor->size; j++) 
+                                {
+                                    cout << cursor->keys[j].value << " ";
+                                    // for (int k = 0; k < cursor->keys[j].add_vect.size(); ++j) 
+                                    // {
+                                    //     if(preholder != cursor->keys[j].value)
+                                    //     {
+                                    //         preholder = cursor->keys[j].value;
+                                    //         printonce =0;
+                                    //     }
+                                    //     if(printonce < 1)
+                                    //     {
+                                    //         printf("\n");
+                                    //         printf("In this index node :");
+                                    //         cout << cursor->keys[j].value << " ";
+                                    //         printf("tconst: ");
+                                    //     }
+                                    countingnode++;
+                                    //     printonce++;
+                                    // }
+                                    
+                                }
+                                cout << "\n";
+                               
+                            } 
+                       //cout << to_string(countingnode) << endl;     
+                    }   
                     cursor = cursor->ptr[i];
                     break;
                 }
                 if(i == cursor->size - 1) // if reach end of keys in this node
                 {
+                    if(flag == true)
+                    {   
+                        countingnode = 0;
+                        //if key is more than the value in the cursor
+                        if(key > cursor->keys[i].value)
+                        {
+                            if(preholder == 0)
+                            {
+                                preholder++;
+                            }
+                            else
+                            {   
+                                for (int j = 0; j < cursor->size; j++) 
+                                {
+                                    cout << cursor->keys[j].value << " ";
+                                    countingnode++;
+                                }
+                            }
+                        }
+                       
+                         
+                    }
+                   
                     cursor = cursor->ptr[i+1];
                     break;
                 }
@@ -40,6 +102,7 @@ Node* BPTree::search(int key, bool flag, bool rangeflag, int key2){
                 {
                     if(flag == true)
                     {
+                        cout << "\n" << endl;
                         cout << to_string(cursor->keys[i].value) +  " Found!\n";
                         for (int j = 0; j < cursor->keys[i].add_vect.size(); ++j) {
                             if(counter <= 4)
@@ -77,9 +140,9 @@ Node* BPTree::search(int key, bool flag, bool rangeflag, int key2){
                             sumDataBlock++;
                         }
                }
-                cout << to_string((avgTotalAvgRating/sumDataBlock)) <<endl;
-                cout << to_string(sumDataBlock) << endl;
-                cout << to_string(cursor->keys[i].add_vect.size())<<endl;
+                // cout << to_string((avgTotalAvgRating/sumDataBlock)) <<endl;
+                // cout << to_string(sumDataBlock) << endl;
+                // cout << to_string(cursor->keys[i].add_vect.size())<<endl;
            }
         }
         
