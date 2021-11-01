55de4cb7576cb42c6c966d2e730216d3eab2d196
ShuHf <fang_shu_hui@outlook.com>
2021-11-01 23:41:22 +0800

    Added validateconnect to test login credential, save it to json the connect will then read from the json. Remove the validate since not using

--- Project2/preprocessing.py
+++ Project2/preprocessing.py
@@ -6,16 +6,41 @@ import sqlparse
 import json
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
+def validateconnect(username, password, database_name):
+    conn = None
+    try:
+        conn = psycopg2.connect(database=database_name, user=username, password=password)
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        verify = True
+        info = {
+            "username" : username,
+            "password" : password,
+            "database" : database_name
+        }
+        json_object = json.dumps(info, indent=4)
+        with open("logininfo.json", "w") as outputfile:
+            outputfile.write(json_object)
+    except (Exception, psycopg2.DatabaseError) as error:
+        verify = False
+    return verify
+
 
 def connect():
     """ Connect to the PostgreSQL database server """
     conn = None
-    database_name = 'cz4031'
+
+    f = open('logininfo.json', "r")
+    data = json.load(f)
+    username = data['username']
+    pwd = data['password']
+    database_name = data['database']
+    print(username, pwd, database_name)
     try:
         # read connection parameters
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(database=database_name, user="postgres", password='root')
+        #username = postgres, pwd = root
+        conn = psycopg2.connect(database=database_name, user=username, password=pwd)
         conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
         # create a cursor
@@ -24,15 +49,7 @@ def connect():
         print(error)
     finally:
         return cur
-def validation(text):
-    if not text:
-        return "Empty string"
-    else:
-        sqlstatement = sqlparse.split(text)
-        # if not sqlstatement.is_valid():
-        #     return sqlstatement.errors
-        # else:
-        #     return text
+
 def executeQuery(text):
     newStatement = "EXPLAIN (analyze, verbose, costs, format JSON) " + text
     cursor = connect()
