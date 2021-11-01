5ba55a56428f2e97039cd0deef63d9dd1c57448e
ShuHf <fang_shu_hui@outlook.com>
2021-11-01 23:39:51 +0800

    Modify to run the gui starting with the username popup. commented out the reading to csv

--- Project2/project.py
+++ Project2/project.py
@@ -7,26 +7,25 @@ import pandas as pd
 from interface import *
 
 #Remove last "|" from each line, in testing
-def fileProcessing():
-    files = [["csv/customer.csv", "customer.csv" ], ["csv/lineitem.csv","lineitem.csv"], ["csv/nation.csv", "nation.csv"],
-             ["csv/orders.csv", "orders.csv"], ["csv/part.csv","part.csv"], ["csv/partsupp.csv","partsupp.csv"],
-             ["csv/region.csv","region.csv"], ["csv/supplier.csv","supplier.csv"]]
-    for filename,output in files:
-        rows = list()
-        file = open(filename, "r")
-        lines = file.read().split("\n")
-
-        for line in lines:
-            rows.append(line[:-1])
-
-        df = pd.DataFrame(rows)
-        df.to_csv("edit/" + output, header=False, index=False)
-        file.close()
+# def fileProcessing():
+#     files = [["csv/customer.csv", "customer.csv" ], ["csv/lineitem.csv","lineitem.csv"], ["csv/nation.csv", "nation.csv"],
+#              ["csv/orders.csv", "orders.csv"], ["csv/part.csv","part.csv"], ["csv/partsupp.csv","partsupp.csv"],
+#              ["csv/region.csv","region.csv"], ["csv/supplier.csv","supplier.csv"]]
+#     for filename,output in files:
+#         rows = list()
+#         file = open(filename, "r")
+#         lines = file.read().split("\n")
+#
+#         for line in lines:
+#             rows.append(line[:-1])
+#
+#         df = pd.DataFrame(rows)
+#         df.to_csv("edit/" + output, header=False, index=False)
+#         file.close()
 
 if __name__ == "__main__":
-        gui = interface()
-        gui.gui()
         print("Welcome to CZ4031 database gui")
+        gui = main_account_screen()
         #fileProcessing()
 
 
