32e149928836a0abedb807c96db9e73978098fcc
Loh Xin Yi <lohxinyi135@gmail.com>
2021-11-02 20:59:51 +0800

    Update annotation.py

--- Project2/annotation.py
+++ Project2/annotation.py
@@ -1,1 +1,15 @@
 #generating the annotations
+import json
+
+Node_Descriptions = {
+    'Seq Scan': 'The tables are read using sequential scan.',
+    'Hash Join': 'This join is implemented by the hash join operator.'
+}
+
+# extract "Node Type" from json file
+with open('queryplan_select.json') as json_file:
+    data = json.loads(json_file)
+    type = data["Node Type"]
+    print(type)
+
+# generate annotations with given Node Descriptions
