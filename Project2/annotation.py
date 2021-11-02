db47cf30445db904316dc3ccfb29f1122e62b1ac
Loh Xin Yi <lohxinyi135@gmail.com>
2021-11-02 21:38:25 +0800

    add get_node_type func

--- Project2/annotation.py
+++ Project2/annotation.py
@@ -7,9 +7,19 @@ Node_Descriptions = {
 }
 
 # extract "Node Type" from json file
-with open('queryplan_select.json') as json_file:
-    data = json.loads(json_file)
-    type = data["Node Type"]
-    print(type)
+def get_node_type():
+    with open('queryplan_select.json') as json_file:
+        data = json.loads(json_file)
+        type = data["Node Type"]
+        print(type)
+
+    return type
+
+
+# get node description based on node type
+def get_node_description(type):
+    description = ''
+    if type in Node_Descriptions:
+        description = Node_Descriptions[type]
 
 # generate annotations with given Node Descriptions
