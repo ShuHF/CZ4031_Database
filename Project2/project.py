3442c295711d47c288176353bb4ae80ecf090965
musifahamran <49061096+musifahamran@users.noreply.github.com>
2021-11-12 12:56:12 +0800

    Update project.py

--- Project2/project.py
+++ Project2/project.py
@@ -7,32 +7,10 @@ import csv
 
 
 
-# #Remove last "|" from each line, in testing
-
-# def fileProcessing():
-#     files = [["csv/customer.csv", "customer.csv" ], ["csv/lineitem.csv","lineitem.csv"], ["csv/nation.csv", "nation.csv"],
-#              ["csv/orders.csv", "orders.csv"], ["csv/part.csv","part.csv"], ["csv/partsupp.csv","partsupp.csv"],
-#              ["csv/region.csv","region.csv"], ["csv/supplier.csv","supplier.csv"]]
-#     for filename,output in files:
-#         rows = list()
-#         file = open(filename, "r")
-#         lines = file.read().split("\n")
-
-#
-#         for line in lines:
-#             rows.append(line[:-1])
-#
-
-#         df = pd.DataFrame(rows)
-#         df.to_csv("edit/" + output, header=False, index=False)
-#         file.close()
-
-
 if __name__ == "__main__":
     print("Welcome to CZ4031 database gui")
     from interface import main_account_screen
     main_account_screen()
-         #fileProcessing()
 
 
 
